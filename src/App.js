3e6ed191dbf80a0d9f633af24a4e743f9f3c98e6
Vibhor Singh <vibhor.formal@gmail.com>
2020-10-09 18:58:02 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
 function App() {
   return (
     <div className="App">
