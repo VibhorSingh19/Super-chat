6f228258a0d67f8fcfd22a7b4f4c3256a13165bf
Vibhor Singh <vibhor.formal@gmail.com>
2020-10-09 19:11:20 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -6,7 +6,15 @@ import 'firebase/firestore';
 import 'firebase/auth';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
+/*<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="/__/firebase/7.23.0/firebase-app.js"></script>
 
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="/__/firebase/7.23.0/firebase-analytics.js"></script>
+
+<!-- Initialize Firebase -->
+<script src="/__/firebase/init.js"></script>*/
 firebase.initialiseApp({
 
 })
